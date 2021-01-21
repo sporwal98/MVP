5e819faddbde6644acf85c645dd6b0a1d616de67
sporwal98 <sahaj.porwal@outlook.com>
2021-01-21 14:01:42 +0000

    Add files via upload

--- mcising.py
+++ mcising.py
@@ -1,9 +1,17 @@
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
+import sys
 
-def main(step = 'g',nsteps = 10000, J = 1.0, size = 50, T = 100):
+def main(model = 'g',nsteps = 10000, J = 1.0, size = 50, T = 1.0,efile = 'energies'):
     acc = 0
+    energies = []
+    time = []
+
+    efile = efile+'T'+str(T)+'_model_'+model+'.txt'
+    efile = open(efile,'w')
+    efile.write('Time-step\tEnergy\n')
+    
     
     spins = np.zeros((size,size))
     (r,c) = spins.shape
@@ -20,11 +28,18 @@ def main(step = 'g',nsteps = 10000, J = 1.0, size = 50, T = 100):
     newspins = spins.copy()
 
     for st in range(nsteps):
+        if(np.mod(st,10)==0):
+            print('sweep no.: '+str(st))
+            en = E(spins,J)
+            energies.append(en)
+            time.append(st)
+            efile.write(str(st)+'\t'+str(en)+'\n')
+        
         for sweep in range(size*size):
         #size*size flips each time step, each flip is random
             dE = 0
             #calc proposed change:
-            if step == 'g':
+            if model == 'g':
                 dE,newspins = glauber(spins,J)
             else:
                 dE,newspins = kawasaki(spins,J)
@@ -34,9 +49,15 @@ def main(step = 'g',nsteps = 10000, J = 1.0, size = 50, T = 100):
                 spins = newspins.copy()
                 acc = acc+1
 
-        if(np.mod(st,100)==0):
-            print('sweeps completed: '+str(st))
-    print('acc rate: '+str(100*acc/(nsteps*size*size)))
+    efile.close()
+    plt.cla()
+    plt.plot(time, energies)
+    plt.xlabel('Time-steps')
+    plt.ylabel('Total Energy')
+    
+    plt.savefig('plot_T'+str(T)+'_energies.png')
+    print('T = '+str(T)+' completed')   
+    #print('acc rate: '+str(100*acc/(nsteps*size*size)))
     
     return
 
@@ -88,15 +109,14 @@ def kawasaki(spins,J):
     pos = np.random.random(4) *np.array([r-1,c-1,r-1,c-1])
 
     (i1,j1,i2,j2) = int(np.round(pos[0])),int(np.round(pos[1])),int(np.round(pos[2])),int(np.round(pos[3]))
-    if(i1==i2 & j1==j2):
+    if((i1==i2 & j1==j2)|(spins[i1,j1] == spins[i2,j2])):
         newspins = spins.copy()
     else:
         newspins = spins.copy()
         newspins[i1,j1] = spins[i2,j2]
         newspins[i2,j2] = spins[i1,j1]
+        dE = dE_K(spins,newspins,i1,j1,i2,j2,J)
 
-    
-    dE = dE_K(spins,newspins,i1,j1,i2,j2,J)
     return dE,newspins
 
 def metropolis(dE,T):
