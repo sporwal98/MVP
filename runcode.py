3bf68b6fbc4e0c2e758ae58311f98ba26a7417da
sporwal98 <sahaj.porwal@outlook.com>
2021-01-30 11:16:30 +0000

    Delete runcode.py

--- runcode.py
+++
@@ -1,9 +0,0 @@
-from mcising import *
-
-def run(model,start=1.0, end=3.1, step=0.1):
-    for T in np.arange(start,end, step):
-        main(model = model, T = T)
-
-    print('Complete')
-
-    return
